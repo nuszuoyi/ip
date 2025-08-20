57110a03dd2a13d3fb159546373a64153b9a1b74
zuo yi <e1481832@u.nus.edu>
2025-08-21 01:48:05 +0800

    Mark as Done

--- src/main/java/AmadeusK.java
+++ src/main/java/AmadeusK.java
@@ -1,4 +1,5 @@
-import java.util.Scanner;  
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class AmadeusK {
      public static void printMessage(String message){
@@ -7,29 +8,65 @@ public class AmadeusK {
             
             System.out.println(horizontalLine);
             for (String line : lines) {
-                System.out.println("     " + line.trim());  
+                System.out.println("     " + line);  
             }
             System.out.println(horizontalLine);
             System.out.println();
         }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);  
-        String recordedString = "";
-        int count = 0;
+        ArrayList<Task> tasks = new ArrayList<>();
+        
         printMessage("Hello! I'm AmadeusK\n What can I do for you?");
         String input = sc.nextLine();  
+        
         while (!input.equals("bye")) {
             if (input.equals("list")) {
+                StringBuilder output = new StringBuilder().append("Here are the tasks in your list:\n");
+                for (int i = 0; i < tasks.size(); i++) {
+                    Task task = tasks.get(i);
+                    output.append(i + 1).append(".");
+                    output.append("[" + task.getStatusIcon() + "] ");
+                    output.append(task.getDescription()).append("\n");
+                }
                 
-                printMessage(recordedString);
+                printMessage(output.toString());
                 input = sc.nextLine();
-                
             } 
-            else{
-
-                printMessage("added: "+input);
-                count++;
-                recordedString += Integer.toString(count) + ". " + input + "\n";
+            else if (input.startsWith("mark")) {
+                try {
+                    int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                    if (index >= 0 && index < tasks.size()) {
+                        Task task = tasks.get(index);
+                        task.markAsDone();
+                        printMessage("Nice! I've marked this task as done:\n  [X] " + task.getDescription());
+                    } else {
+                        printMessage("OOPS!!! The task number is invalid");
+                    }
+                } catch (Exception e) {
+                    printMessage("OOPS!!! The task number is invalid");
+                }
+                input = sc.nextLine();
+            }
+            else if (input.startsWith("unmark")) {
+                try {
+                    int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                    if (index >= 0 && index < tasks.size()) {
+                        Task task = tasks.get(index);
+                        task.markAsUndone();
+                        printMessage("OK, I've marked this task as not done yet:\n  [ ] " + task.getDescription());
+                    } else {
+                        printMessage("OOPS!!! The task number is invalid");
+                    }
+                } catch (Exception e) {
+                    printMessage("OOPS!!! The task number is invalid");
+                }
+                input = sc.nextLine();
+            }
+            else {
+                Task newTask = new Task(input);
+                tasks.add(newTask);
+                printMessage("added: " + input);
                 input = sc.nextLine(); 
             }
         }
@@ -37,3 +74,4 @@ public class AmadeusK {
         sc.close(); 
     }
 }
+
