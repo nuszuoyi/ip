fd95ea7f579de215052b0bf6e44fc851d7b30787
zuo yi <e1481832@u.nus.edu>
2025-08-21 01:00:42 +0800

    Echo

--- src/main/java/AmadeusK.java
+++ src/main/java/AmadeusK.java
@@ -1,11 +1,27 @@
+import java.util.Scanner;  
+
 public class AmadeusK {
+     public static void printMessage(String message){
+            String horizontalLine= "    ____________________________________________________________";
+            String[] lines = message.split("\n");  
+            
+            System.out.println(horizontalLine);
+            for (String line : lines) {
+                System.out.println("     " + line.trim());  
+            }
+            System.out.println(horizontalLine);
+            System.out.println();
+        }
     public static void main(String[] args) {
-        String horizontalLine= "____________________________________________________________";
-        System.out.println(horizontalLine);
-        System.out.println("Hello! I'm AmadeusK\n"+"What can I do for you?");
-        System.out.println(horizontalLine);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(horizontalLine);
+        Scanner sc = new Scanner(System.in);  
         
+        printMessage("Hello! I'm AmadeusK\n What can I do for you?");
+        String input = sc.nextLine();  
+        while (!input.equals("bye")) {  
+            printMessage(input);
+            input = sc.nextLine(); 
+        }
+        printMessage("Bye. Hope to see you again soon!");
+        sc.close(); 
     }
 }
