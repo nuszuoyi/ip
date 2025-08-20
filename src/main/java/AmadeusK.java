2c073a0d938730af78ffc84e9b66f779ecbd5937
zuo yi <e1481832@u.nus.edu>
2025-08-21 00:43:06 +0800

    Rename, Greet, Exit

---
+++ src/main/java/AmadeusK.java
@@ -0,0 +1,11 @@
+public class AmadeusK {
+    public static void main(String[] args) {
+        String horizontalLine= "____________________________________________________________";
+        System.out.println(horizontalLine);
+        System.out.println("Hello! I'm AmadeusK\n"+"What can I do for you?");
+        System.out.println(horizontalLine);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontalLine);
+        
+    }
+}
